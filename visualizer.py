0ccf8caaa090bbee667638f9999bd14633490883
Hritesh Bhardwaj <honeybhardwaj1001@gmail.com>
2022-05-20 13:15:18 +0530

    Necessary Global

---
+++ visualizer.py
@@ -0,0 +1,18 @@
+from turtle import update
+import pygame
+import random
+pygame.init()
+
+class GlobalVariable:
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
+    GREEN = 0, 255, 0
+    RED = 255, 0, 0
+    BG_COLOR = WHITE
+    SIDE_PAD = 100
+    TOP_PAD = 150
+    GRADIENT = [
+		(128, 128, 128),
+		(160, 160, 160),
+		(192, 192, 192)
+	]
