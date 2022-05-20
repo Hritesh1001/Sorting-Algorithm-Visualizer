921bf206bef21e45761bd443c27453476d0a400b
Hritesh Bhardwaj <honeybhardwaj1001@gmail.com>
2022-05-20 13:18:48 +0530

    Global Complete

--- visualizer.py
+++ visualizer.py
@@ -16,3 +16,20 @@ class GlobalVariable:
 		(160, 160, 160),
 		(192, 192, 192)
 	]
+    
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+        
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visualizer")
+        self.set_list(lst)
+    
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
+        
